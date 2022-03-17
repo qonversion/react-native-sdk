f8d564753009c40a2b8e786106e8dd75b2237cc8
Kamo Spertsyan <spertsiankamo@gmail.com>
2022-03-17 22:50:15 +0300

    Automation fixes after testing and notifications environment for sample. (#104)
    * Android push notifications implemented.
    
    * iOS notifications supported.
    
    * Android automation fixes.
    
    * iOS automation fixes.

---
+++ example/notificationsManager.js
@@ -0,0 +1,42 @@
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import PushNotification from 'react-native-push-notification';
+import Qonversion from 'react-native-qonversion';
+import {Platform} from 'react-native';
+
+const NotificationsManager = {
+  init: () => {
+    PushNotification.configure({
+      onRegister: function(token) {
+        console.log('Device token:', token);
+        Qonversion.setNotificationsToken(token.token);
+      },
+
+      onNotification: async function(notification) {
+        console.log('Notification:', notification);
+
+        if (notification.userInteraction) {
+          let notificationData = '';
+          if (Platform.OS === 'ios') {
+            notificationData = notification.data.aps;
+          } else if (Platform.OS === 'android') {
+            notificationData = JSON.parse(notification.data.notificationData);
+          }
+          const isQonversionNotification = await Qonversion.handleNotification(
+            notificationData,
+          );
+          console.log(
+            'Is notification handled by Qonversion: ',
+            isQonversionNotification,
+          );
+          if (!isQonversionNotification) {
+            // handle push notification yourself
+          }
+        }
+
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+    });
+  },
+};
+
+export default NotificationsManager;
