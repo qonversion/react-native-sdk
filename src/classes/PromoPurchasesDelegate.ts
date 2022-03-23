2dbb30e9136537e17f3bf06fb01af7d953375661
Kamo Spertsyan <spertsiankamo@gmail.com>
2022-03-23 18:24:45 +0300

    Update PromoPurchasesDelegate.ts

--- src/classes/PromoPurchasesDelegate.ts
+++ src/classes/PromoPurchasesDelegate.ts
@@ -4,10 +4,10 @@ export interface PromoPurchasesDelegate {
 
   /**
    * Delegate fires each time a promo purchase from the App Store happens.
-   * Call startPromoPurchase in case of your app is ready to start promo purchase.
-   * Or cache that delegate and call later when you need.
+   * Call promoPurchaseExecutor in case of your app is ready to start promo purchase.
+   * Or cache that executor and call later when you need.
    * @param productId StoreKit product identifier.
-   * @param promoPurchaseExecutor A delegate that will start a promo purchase flow.
+   * @param promoPurchaseExecutor a function that will start a promo purchase flow.
    */
   onPromoPurchaseReceived(productId: string, promoPurchaseExecutor: () => Promise<Map<string, Permission>>): void;
 }
