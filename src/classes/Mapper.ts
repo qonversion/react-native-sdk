27fac5e80c27c9b7f2dfcea903fcd5ebf75e8a88
kamospertsyan <spertsiankamo@gmail.com>
2022-04-21 18:55:30 +0300

    isDownloadable fix.

--- src/classes/Mapper.ts
+++ src/classes/Mapper.ts
@@ -84,7 +84,7 @@ type QSKProduct = {
   price: string;
   priceLocale: QLocale;
   productIdentifier: string | undefined;
-  isDownloadable: boolean;
+  isDownloadable: boolean | undefined;
   downloadContentVersion: string | undefined;
   downloadContentLengths: number[] | undefined;
   productDiscount: SKProductDiscount | undefined;
@@ -362,7 +362,7 @@ class Mapper {
       skProduct.price,
       skProduct.priceLocale.localeIdentifier,
       skProduct.productIdentifier,
-      skProduct.isDownloadable,
+      !!skProduct.isDownloadable,
       skProduct.downloadContentVersion,
       skProduct.downloadContentLengths,
       subscriptionPeriod,
