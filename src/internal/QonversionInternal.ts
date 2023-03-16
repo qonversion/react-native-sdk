1cb961d0f8d4584740b15adfde7fab36994913ba
Surik Sarkisyan <suriksarkisyan@users.noreply.github.com>
2023-03-16 21:19:14 +0400

    Added syncHistoricalData logic (#224)
    * Added syncHistoricalData logic
    
    * Added missed return type
    
    * Semicolon fixes
    
    ---------
    
    Co-authored-by: Surik <action@github.com>

--- src/internal/QonversionInternal.ts
+++ src/internal/QonversionInternal.ts
@@ -30,11 +30,15 @@ export default class QonversionInternal implements QonversionApi {
       qonversionConfig.entitlementsCacheLifetime,
       qonversionConfig.proxyUrl,
       qonversionConfig.kidsMode
-    )
+    );
 
     if (qonversionConfig.entitlementsUpdateListener) {
-      this.setEntitlementsUpdateListener(qonversionConfig.entitlementsUpdateListener)
+      this.setEntitlementsUpdateListener(qonversionConfig.entitlementsUpdateListener);
     }
+  }
+
+  syncHistoricalData () {
+    RNQonversion.syncHistoricalData();
   }
 
   async purchase(productId: string): Promise<Map<string, Entitlement>> {
