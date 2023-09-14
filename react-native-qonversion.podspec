661e0170f823bdada03acfb01c7d5d3dcb62cb8c
Surik <action@github.com>
2023-09-14 19:28:02 +0400

    Added remote configuration source logic

--- react-native-qonversion.podspec
+++ react-native-qonversion.podspec
@@ -22,5 +22,5 @@ Pod::Spec.new do |s|
   s.requires_arc = true
 
   s.dependency "React"
-  s.dependency "QonversionSandwich", "3.0.1"
+  s.dependency "QonversionSandwich", "3.1.0"
 end
---
+++ src/dto/RemoteConfigurationSource.ts
@@ -0,0 +1,19 @@
+import ExperimentGroup from "./ExperimentGroup";
+import {RemoteConfigurationAssignmentType, RemoteConfigurationSourceType} from "./enums";
+
+
+class RemoteConfigurationSource {
+    id: string;
+    name: string;
+    type: RemoteConfigurationSourceType;
+    assignmentType: RemoteConfigurationAssignmentType;
+
+    constructor(id: string, name: string, type: RemoteConfigurationSourceType, assignmentType: RemoteConfigurationAssignmentType) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.assignmentType = assignmentType;
+    }
+}
+
+export default RemoteConfigurationSource;
