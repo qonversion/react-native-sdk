52c476b2de33ce711ca800bb080c99d881fe8b5b
SpertsyanKM <SpertsyanKM@users.noreply.github.com>
2024-07-29 10:32:35 +0000

    [create-pull-request] automated change

--- src/internal/QonversionInternal.ts
+++ src/internal/QonversionInternal.ts
@@ -19,7 +19,7 @@ import {RemoteConfigList} from '../index';
 
 const {RNQonversion} = NativeModules;
 
-const sdkVersion = "8.0.0";
+const sdkVersion = "7.5.2";
 
 const EVENT_ENTITLEMENTS_UPDATED = "entitlements_updated";
 const EVENT_PROMO_PURCHASE_RECEIVED = "promo_purchase_received";
