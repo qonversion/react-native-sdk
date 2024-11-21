38071ab2c6cfd82a7168d879bb822a8de186c0f7
suriksarkisyan <suriksarkisyan@users.noreply.github.com>
2024-11-21 17:00:19 +0000

    [create-pull-request] automated change

--- src/internal/QonversionInternal.ts
+++ src/internal/QonversionInternal.ts
@@ -23,7 +23,7 @@ import PromotionalOffer from '../dto/PromotionalOffer';
 
 const {RNQonversion} = NativeModules;
 
-const sdkVersion = "8.1.5";
+const sdkVersion = "8.2.0";
 
 const EVENT_ENTITLEMENTS_UPDATED = "entitlements_updated";
 const EVENT_PROMO_PURCHASE_RECEIVED = "promo_purchase_received";
