b1f05860eb417ce6bedab04c69db12ddfc22fd88
Kamo Spertsyan <spertsiankamo@gmail.com>
2025-01-16 11:35:03 +0300

    Fixed remote config payload type.

--- src/dto/RemoteConfig.ts
+++ src/dto/RemoteConfig.ts
@@ -2,11 +2,11 @@ import Experiment from "./Experiment";
 import RemoteConfigurationSource from "./RemoteConfigurationSource";
 
 class RemoteConfig {
-    payload: Map<string, Object>;
+    payload: Record<string, Object>;
     experiment?: Experiment | null;
     source: RemoteConfigurationSource;
 
-    constructor(payload: Map<string, Object>, experiment: Experiment | null, source: RemoteConfigurationSource) {
+    constructor(payload: Record<string, Object>, experiment: Experiment | null, source: RemoteConfigurationSource) {
         this.payload = payload;
         this.experiment = experiment;
         this.source = source;
