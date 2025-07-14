5bd9bb36927abe1e165eaee27a6d9588f931b0b2
SpertsyanKM <SpertsyanKM@users.noreply.github.com>
2025-07-14 15:27:47 +0000

    [create-pull-request] automated change

--- src/internal/QonversionInternal.ts
+++ src/internal/QonversionInternal.ts
@@ -23,7 +23,7 @@ import PromotionalOffer from '../dto/PromotionalOffer';
 
 const {RNQonversion} = NativeModules;
 
-const sdkVersion = "9.0.1";
+const sdkVersion = "9.0.2";
 
 const EVENT_ENTITLEMENTS_UPDATED = "entitlements_updated";
 const EVENT_PROMO_PURCHASE_RECEIVED = "promo_purchase_received";
